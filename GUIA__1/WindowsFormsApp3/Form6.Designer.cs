25628a5100cbcd4070364e49cc2258a396c1b7b0
Andrew Montoya <44888081+Andreww470000Ranger@users.noreply.github.com>
2019-04-22 23:15:50 -0600

    Guía evaluada de la tercera evaluación
    Guía número 1 de PROGRAMACIÓN

---
+++ GUIA__1/WindowsFormsApp3/Form6.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
+            this.picBoxPB = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxPB
+            // 
+            this.picBoxPB.Location = new System.Drawing.Point(-2, 256);
+            this.picBoxPB.Name = "picBoxPB";
+            this.picBoxPB.Size = new System.Drawing.Size(671, 57);
+            this.picBoxPB.TabIndex = 0;
+            this.picBoxPB.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-89, -179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(758, 443);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 312);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picBoxPB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form6";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form6";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxPB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ GUIA__1/WindowsFormsApp3/Form6.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form6 : Form
+    {
+        Timer T = new Timer();
+        double pbUnit;
+        int pbWIDTH, pbHEIGHT, pbCOMPLETE;
+
+        Bitmap bmp;
+        Graphics g;
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            pbWIDTH = picBoxPB.Width;
+            pbHEIGHT = picBoxPB.Height;
+
+            pbUnit = pbWIDTH / 100.0;
+
+            pbCOMPLETE = 0;
+
+            bmp = new Bitmap(pbWIDTH, pbHEIGHT);
+
+            T.Interval = 50;
+            T.Tick += new EventHandler(this.T_Tick);
+            T.Start();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+
+            g = Graphics.FromImage(bmp);
+
+            g.Clear(Color.Black);
+            g.FillRectangle(Brushes.Gray, new Rectangle(0, 0, (int)(pbCOMPLETE * pbUnit), pbHEIGHT));
+
+            g.DrawString(pbCOMPLETE + "% Cargando", new Font("Helvetica", pbHEIGHT / 2), Brushes.White, new PointF(pbWIDTH / 2 - pbHEIGHT, pbHEIGHT / 10));
+
+            picBoxPB.Image = bmp;
+            //player.controls.play();
+            pbCOMPLETE++;
+            if (pbCOMPLETE > 100)
+            {
+                g.Dispose();
+                T.Stop();
+                this.Hide();
+                MDIParent1 Md = new MDIParent1();
+                Md.Show();
+            }
+
+        }
+
+    }
+}
